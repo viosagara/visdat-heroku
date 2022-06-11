db08eb9286f891dac8fb3d10e74ba8d962673c18
viosagara <vioperkasa347@gmail.com>
2022-06-11 14:12:31 +0700

    update file

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bokeh serve — port=$PORT — allow-websocket-origin=myapp.herokuapp.com — address=0.0.0.0 — use-xheaders sahamvisdat.py
+web: bokeh serve — port=$PORT — allow-websocket-origin=myapp.herokuapp.com — address=0.0.0.0 — use-xheaders visdat-saham.py
--- visdat-saham.py
+++ visdat-saham.py
@@ -115,7 +115,7 @@
 # In[5]:
 
 
-bokeh serve --show myapp.py
+bokeh serve --show visdat-saham.py
 
 
 # For more on all things interaction in Bokeh, [**Adding Interactions**](https://docs.bokeh.org/en/latest/docs/user_guide/interaction.html) in the Bokeh User Guide is a great place to start.
