a184d3bf26be64849e7de3e26ac9426617e3c6a3
viosagara <vioperkasa347@gmail.com>
2022-06-11 14:04:37 +0700

    Update sahamvisdat.py

--- sahamvisdat.py
+++ sahamvisdat.py
@@ -21,95 +21,95 @@ from bokeh.models import Slider, Select
 # In[2]:
 
 
-data = pd.read_csv("./data/gapminder_tidy.csv")
-data.set_index('Year', inplace=True)
+# data = pd.read_csv("./data/gapminder_tidy.csv")
+# data.set_index('Year', inplace=True)
 
 
-# In[3]:
+# # In[3]:
 
 
-# Make a list of the unique values from the region column: regions_list
-regions_list = data.region.unique().tolist()
+# # Make a list of the unique values from the region column: regions_list
+# regions_list = data.region.unique().tolist()
 
-# Make a color mapper: color_mapper
-color_mapper = CategoricalColorMapper(factors=regions_list, palette=Spectral6)
-
-
-# In[4]:
+# # Make a color mapper: color_mapper
+# color_mapper = CategoricalColorMapper(factors=regions_list, palette=Spectral6)
 
 
-# Make the ColumnDataSource: source
-source = ColumnDataSource(data={
-    'x'       : data.loc[1970].fertility,
-    'y'       : data.loc[1970].life,
-    'country' : data.loc[1970].Country,
-    'pop'     : (data.loc[1970].population / 20000000) + 2,
-    'region'  : data.loc[1970].region,
-})
+# # In[4]:
 
 
-# In[ ]:
+# # Make the ColumnDataSource: source
+# source = ColumnDataSource(data={
+#     'x'       : data.loc[1970].fertility,
+#     'y'       : data.loc[1970].life,
+#     'country' : data.loc[1970].Country,
+#     'pop'     : (data.loc[1970].population / 20000000) + 2,
+#     'region'  : data.loc[1970].region,
+# })
 
 
-# Create the figure: plot
-plot = figure(title='1970', x_axis_label='Fertility (children per woman)', y_axis_label='Life Expectancy (years)',
-           plot_height=400, plot_width=700, tools=[HoverTool(tooltips='@country')])
+# # In[ ]:
 
-# Add a circle glyph to the figure p
-plot.circle(x='x', y='y', source=source, fill_alpha=0.8,
-           color=dict(field='region', transform=color_mapper), legend='region')
 
-# Set the legend and axis attributes
-plot.legend.location = 'bottom_left'
+# # Create the figure: plot
+# plot = figure(title='1970', x_axis_label='Fertility (children per woman)', y_axis_label='Life Expectancy (years)',
+#            plot_height=400, plot_width=700, tools=[HoverTool(tooltips='@country')])
 
-# Define the callback function: update_plot
-def update_plot(attr, old, new):
-    # set the `yr` name to `slider.value` and `source.data = new_data`
-    yr = slider.value
-    x = x_select.value
-    y = y_select.value
-    # Label axes of plot
-    plot.xaxis.axis_label = x
-    plot.yaxis.axis_label = y
-    # new data
-    new_data = {
-    'x'       : data.loc[yr][x],
-    'y'       : data.loc[yr][y],
-    'country' : data.loc[yr].Country,
-    'pop'     : (data.loc[yr].population / 20000000) + 2,
-    'region'  : data.loc[yr].region,
-    }
-    source.data = new_data
+# # Add a circle glyph to the figure p
+# plot.circle(x='x', y='y', source=source, fill_alpha=0.8,
+#            color=dict(field='region', transform=color_mapper), legend='region')
+
+# # Set the legend and axis attributes
+# plot.legend.location = 'bottom_left'
+
+# # Define the callback function: update_plot
+# def update_plot(attr, old, new):
+#     # set the `yr` name to `slider.value` and `source.data = new_data`
+#     # yr = slider.value
+#     # x = x_select.value
+#     # y = y_select.value
+#     # # Label axes of plot
+#     # plot.xaxis.axis_label = x
+#     # plot.yaxis.axis_label = y
+#     # # new data
+#     # new_data = {
+#     # 'x'       : data.loc[yr][x],
+#     # 'y'       : data.loc[yr][y],
+#     # 'country' : data.loc[yr].Country,
+#     # 'pop'     : (data.loc[yr].population / 20000000) + 2,
+#     # 'region'  : data.loc[yr].region,
+#     # }
+#     # source.data = new_data
     
     # Add title to figure: plot.title.text
-    plot.title.text = 'Gapminder data for %d' % yr
+    # plot.title.text = 'Gapminder data for %d' % yr
 
 # Make a slider object: slider
-slider = Slider(start=1970, end=2010, step=1, value=1970, title='Year')
-slider.on_change('value',update_plot)
+# slider = Slider(start=1970, end=2010, step=1, value=1970, title='Year')
+# slider.on_change('value',update_plot)
 
 # Make dropdown menu for x and y axis
 # Create a dropdown Select widget for the x data: x_select
-x_select = Select(
-    options=['fertility', 'life', 'child_mortality', 'gdp'],
-    value='fertility',
-    title='x-axis data'
-)
+# x_select = Select(
+#     options=['fertility', 'life', 'child_mortality', 'gdp'],
+#     value='fertility',
+#     title='x-axis data'
+# )
 # Attach the update_plot callback to the 'value' property of x_select
-x_select.on_change('value', update_plot)
+# x_select.on_change('value', update_plot)
 
 # Create a dropdown Select widget for the y data: y_select
-y_select = Select(
-    options=['fertility', 'life', 'child_mortality', 'gdp'],
-    value='life',
-    title='y-axis data'
-)
+# y_select = Select(
+#     options=['fertility', 'life', 'child_mortality', 'gdp'],
+#     value='life',
+#     title='y-axis data'
+# )
 # Attach the update_plot callback to the 'value' property of y_select
-y_select.on_change('value', update_plot)
+# y_select.on_change('value', update_plot)
     
 # Create layout and add to current document
-layout = row(widgetbox(slider, x_select, y_select), plot)
-curdoc().add_root(layout)
+# layout = row(widgetbox(slider, x_select, y_select), plot)
+# curdoc().add_root(layout)
 
 
 # In[5]:
