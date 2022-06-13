a1ee35bd2b98bb9974b8ab7ddbab9e6fb022067a
viosagara <vioperkasa347@gmail.com>
2022-06-13 20:30:16 +0700

    Update main.py

--- visdatsaham/main.py
+++ visdatsaham/main.py
@@ -1,44 +1,25 @@
-# # myapp.py
+from math import pi
 
-# from random import random
+import pandas as pd
 
-# from bokeh.layouts import column
-# from bokeh.models import Button
-# from bokeh.palettes import RdYlBu3
-# from bokeh.plotting import figure, curdoc
+from bokeh.plotting import figure, show
+from bokeh.sampledata.stocks import MSFT
 
-# # create a plot and style its properties
-# p = figure(x_range=(0, 100), y_range=(0, 100), toolbar_location=None)
-# p.border_fill_color = 'black'
-# p.background_fill_color = 'black'
-# p.outline_line_color = None
-# p.grid.grid_line_color = None
+df = pd.DataFrame(MSFT)[:50]
+df["date"] = pd.to_datetime(df["date"])
 
-# # add a text renderer to the plot (no data yet)
-# r = p.text(x=[], y=[], text=[], text_color=[], text_font_size="26px",
-#            text_baseline="middle", text_align="center")
+inc = df.close > df.open
+dec = df.open > df.close
+w = 12*60*60*1000 # half day in ms
 
-# i = 0
+TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
 
-# ds = r.data_source
+p = figure(x_axis_type="datetime", tools=TOOLS, width=1000, title = "MSFT Candlestick")
+p.xaxis.major_label_orientation = pi/4
+p.grid.grid_line_alpha=0.3
 
-# # create a callback that adds a number in a random location
-# def callback():
-#     global i
+p.segment(df.date, df.high, df.date, df.low, color="black")
+p.vbar(df.date[inc], w, df.open[inc], df.close[inc], fill_color="#D5E1DD", line_color="black")
+p.vbar(df.date[dec], w, df.open[dec], df.close[dec], fill_color="#F2583E", line_color="black")
 
-#     # BEST PRACTICE --- update .data in one step with a new dict
-#     new_data = dict()
-#     new_data['x'] = ds.data['x'] + [random()*70 + 15]
-#     new_data['y'] = ds.data['y'] + [random()*70 + 15]
-#     new_data['text_color'] = ds.data['text_color'] + [RdYlBu3[i%3]]
-#     new_data['text'] = ds.data['text'] + [str(i)]
-#     ds.data = new_data
-
-#     i = i + 1
-
-# # add a button widget and configure with the call back
-# button = Button(label="Press Me")
-# button.on_click(callback)
-
-# # put the button and plot in a layout and add to the document
-# curdoc().add_root(column(button, p))
+show(p)
